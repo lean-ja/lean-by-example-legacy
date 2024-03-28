5a6a282109fa1f48029635100b7d28e187d2b25c
Seasawher <tellagoodstory@icloud.com>
2024-03-29 01:57:47 +0900

    Coe を追加する

---
+++ Src/Coe.lean
@@ -0,0 +1,28 @@
+/-
+# Coe
+`Coe` は，型強制(coercion)と呼ばれる仕組みをユーザが定義するための型クラスです．
+
+ある型 `T` が期待される場所に別の型の項 `s : S` を見つけると，Lean は型エラーにする前に自動的に型変換を行うことができないか試します．ここで行われる「自動的な型変換」が型強制です．
+
+例えば，正の自然数からなる型 `Pos` を定義したとします．包含関係から `Pos → Nat` という変換ができるはずです．この変換を関数として定義するだけでは，必要になるごとに毎回書かなければなりませんが，型強制を使うと自動化することができます．
+-/
+
+inductive Pos where
+  | one : Pos
+  | succ : Pos → Pos
+
+def one : Pos := Pos.one
+
+-- `List.drop` の引数は `Nat` なのに，`Pos` を渡したのでエラーになっている
+#check_failure [1, 2, 3].drop one
+
+/-- `Pos` から `Nat` へ変換する -/
+def Pos.toNat : Pos → Nat
+  | one => 1
+  | succ n => n.toNat + 1
+
+instance : Coe Pos Nat where
+  coe n := n.toNat
+
+-- 自動的に `Pos` から `Nat` への変換が行われてエラーにならない
+#check [1, 2, 3].drop one
--- md/SUMMARY.md
+++ md/SUMMARY.md
@@ -4,10 +4,19 @@
 
 ---
 
+# 構文・コマンド
+
 * [`_root_`: 一時的に名前空間を抜ける](./build/Root.md)
 * [abbrev: 別名を定義する](./build/Abbrev.md)
 * [namespace: 名前空間を定義する](./build/Namespace.md)
 * [protected: フルネームを強制する](./build/Protected.md)
+
+# 型クラス
+
+* [Coe: 型強制](./build/Coe.md)
+
+# コラム
+
 * [コメント](./Comment.md)
 * [ドット記法](./build/DotNotation.md)
 * [パイプライン演算子](./build/Pipeline.md)
