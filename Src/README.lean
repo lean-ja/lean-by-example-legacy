c9cf611a6b1591e2d7a67738eacc1d4dc5355df1
Seasawher <tellagoodstory@icloud.com>
2024-03-30 13:48:21 +0900

    README を更新

--- Src/README.lean
+++ Src/README.lean
@@ -1,1 +1,6 @@
-/- # Lean by Example -/
+/- # Lean by Example
+
+Lean by Example は，Lean の基本的な構文や機能を紹介するためのサンプルコード集です．Lean の数多くの機能の全体を概観できる資料となることを目指しています．
+
+なお本書は[逆引きタクティクリスト](https://lean-ja.github.io/tactic-cheatsheet/)の姉妹編です．タクティクとコマンドについてはあちらを参照してください．
+-/
