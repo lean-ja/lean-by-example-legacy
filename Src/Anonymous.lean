bb2e5b8ee9e19f8404edbda9672e8a0b99fcb176
Seasawher <tellagoodstory@icloud.com>
2024-04-17 03:04:22 +0900

    匿名コンストラクタという言葉の指す対象が間違っていたので修正

--- Src/Anonymous.lean
+++ Src/Anonymous.lean
@@ -1,29 +1,30 @@
 /- # 匿名コンストラクタ
-匿名コンストラクタは，帰納型(構造体も含まれます) `T` に対して `T` 型の項を分解（デコンストラクト）する方法のひとつです．
+匿名コンストラクタは，構造体 `T` に対して `T` 型の項を構成する方法のひとつです．
 
-これは以下に示すように，`⟨x1, x2, ...⟩ | ⟨y1, y2, ...⟩ | ..` という構文により使うことができます．
+これは以下に示すように，`⟨x1, x2, ...⟩` という構文により使うことができます．
 -/
 set_option linter.unusedVariables false --#
 
+/-- 2つのフィールドを持つ構造体 -/
+structure Hoge where
+  foo : Nat
+  bar : Nat
+
+def hoge : Hoge := ⟨1, 2⟩
+
+/- コンストラクタが入れ子になっていても平坦化することができます．例えば，以下の2つの定義は同じことをしています．-/
+
+def foo : Nat × (Int × String) := ⟨1, ⟨2, "foo"⟩⟩
+
+def foo' : Nat × Int × String := ⟨1, 2, "foo"⟩
+
+/- 一般の帰納型に対しては使用できません．-/
+
 inductive Sample where
   | fst (foo bar : Nat) : Sample
-  | snd (foo bar : Int) : Sample
+  | snd (foo bar : String) : Sample
 
-theorem sample_triv (s : Sample) : True := by
-  obtain ⟨f1, b1⟩ | ⟨f2, b2⟩ := s
-
-  case fst =>
-    trivial
-
-  case snd =>
-    trivial
-
-/- 匿名コンストラクタを使用しない場合，次のようにコンストラクタ名を指定する必要があります．-/
-
-theorem sample_triv' (s : Sample) : True := by
-  cases s with
-
-  | fst f b =>
-    trivial
-  | snd f b =>
-    trivial
+-- invalid constructor ⟨...⟩,
+-- expected type must be an inductive type with only one constructor
+--   Sample
+#check_failure (⟨"foo", "bar"⟩ : Sample)
