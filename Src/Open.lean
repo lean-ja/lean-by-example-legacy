f6467ea96c7f819b88a76df4cc129ab0383cd66f
Seasawher <tellagoodstory@icloud.com>
2024-04-20 18:11:39 +0900

    Open のコード例を改修

--- Src/Open.lean
+++ Src/Open.lean
@@ -4,21 +4,28 @@
 
 名前空間 `N` の中にある定義 `S` を使いたいとき，通常はフルネームの `N.S` を使う必要がありますが，`open N` とすることで短い別名 `S` でアクセスできるようになります．
 -/
-namespace Open
+namespace Hoge
 
   def foo := "hello"
 
-end Open
+end Hoge
 
 -- 名前空間の外からだと `foo` という短い名前が使えない
 #check_failure foo
 
--- `open` することで `foo` という短い名前が使えるようになる
-open Open
+section
+  -- 名前空間 `Hoge` をオープン
+  open Hoge
 
-#check foo
+  -- `open` することで `foo` という短い名前が使えるようになる
+  #check foo
+end
 
-/- また名前空間 `Foo` 内に `bar` という公理(`axiom` で宣言されたもの)が存在する場合，`Foo` を開くと同時に公理 `Foo.bar` もインポートされます．-/
+-- セクションが終わると再び短い名前は使えなくなる
+#check_failure foo
+
+/- ## 名前空間と公理
+また名前空間 `Foo` 内に `bar` という公理(`axiom` で宣言されたもの)が存在する場合，`Foo` を開くと同時に公理 `Foo.bar` もインポートされます．-/
 
 open Classical
 
