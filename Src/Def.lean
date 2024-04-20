6d3e38595cddee275af00f1bbafa4cf1c92110fa
Seasawher <tellagoodstory@icloud.com>
2024-04-21 02:53:05 +0900

    Def: コロンの使いまわしについて言及

--- Src/Def.lean
+++ Src/Def.lean
@@ -16,6 +16,12 @@ def factorial (n : Nat) : Nat :=
 
 #guard factorial 7 = 5040
 
+/- なお関数を定義するとき，隣接する同じ型の引数に対しては `:` を使いまわすことができます．-/
+
+def add (n m : Nat) : Nat := n + m
+
+def threeAdd (n m l : Nat) : Nat := n + m + l
+
 /- ## partial
 再帰関数も再帰的でない関数と同様に定義することができますが，有限回で終了することを Lean が自動的に証明できないとエラーになります．
 エラーを解消するには，停止性を証明するか，定義に `partial` という修飾子を付けます．-/
