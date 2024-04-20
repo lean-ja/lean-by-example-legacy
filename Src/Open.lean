3c6bb8ebba870acef0d04f1f274efaa7993c3593
Seasawher <tellagoodstory@icloud.com>
2024-04-20 18:19:14 +0900

    open : 入れ子になった名前空間のことを追記

--- Src/Open.lean
+++ Src/Open.lean
@@ -24,6 +24,26 @@ end
 -- セクションが終わると再び短い名前は使えなくなる
 #check_failure foo
 
+/- ## 入れ子になった名前空間
+名前空間 `N₁` と `N₂` が入れ子になっているとき，その下にある定義に短い名前でアクセスするには，`open N₁ N₂` とすればよいです．
+-/
+
+namespace Foo
+
+  namespace Bar
+
+    def baz := "world"
+
+  end Bar
+
+end Foo
+
+-- 入れ子の名前空間を開く
+-- `Foo` の後に `Bar` を開く必要がある
+open Foo Bar
+
+#check baz
+
 /- ## 名前空間と公理
 また名前空間 `Foo` 内に `bar` という公理(`axiom` で宣言されたもの)が存在する場合，`Foo` を開くと同時に公理 `Foo.bar` もインポートされます．-/
 
