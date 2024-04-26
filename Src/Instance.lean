440d58c4c9c4516fd10fe99c6726a335990b89e7
Seasawher <tellagoodstory@icloud.com>
2024-04-27 03:34:18 +0900

    @[instance]について追記する
    resolve #50

--- Src/Instance.lean
+++ Src/Instance.lean
@@ -22,4 +22,21 @@ instance {α : Type} [Add α] : Add (Point α) where
 -- 足し算ができるようになった
 #check (origin + origin)
 
+/-
+## 舞台裏
+`instance` は `@[instance]` タグが付けられた `def` と同じように振る舞います．
+-/
+
+-- `List` 同士を足すことはできない
+#check_failure [1] + [2]
+
+-- インスタンスを宣言する
+@[instance]
+def instListAdd {α : Type} : Add (List α) where
+  add := List.append
+
+-- リスト同士を足すことができるようになった
+-- 実装としては，上で指定した通り `List.append` が使われる
+#check [1] + [2]
+
 end Instance --#
