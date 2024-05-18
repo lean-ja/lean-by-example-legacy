627f3bd7f5d272b49771e0cd2a3d4f2c4429dcb3
Seasawher <tellagoodstory@icloud.com>
2024-05-19 00:01:52 +0900

    section の中にスコープを制限する方法を紹介する

--- Src/Private.lean
+++ Src/Private.lean
@@ -1,10 +1,11 @@
 /- # private
-`private` は，その定義があるファイルの中でだけ参照可能になるようにする修飾子です．
+`private` は，その定義があるファイルの中でだけ参照可能になるようにする修飾子です．他のファイルからはアクセス不能になります．
 
 不安定なAPIなど，外部に公開したくないものに対して使うのが主な用途です．
 
 なお `private` コマンドでセクションや名前空間にスコープを制限することはできません．
 -/
+import Lean --#
 
 namespace Hoge
   section
@@ -17,3 +18,28 @@ open Hoge
 
 -- アクセスできる
 #check addOne
+
+/- 以下の例 [^private] のように，変数のスコープを制限するようなセクションの変種を自作することは可能です． -/
+
+open Lean Elab Command
+
+elab "private section" ppLine cmd:command* ppLine "end private section": command => do
+  let old ← get
+  try
+    for cmd in cmd do
+      elabCommandTopLevel cmd
+    throwAbortCommand
+  finally
+    set old
+
+private section
+  def foo := "hello!!"
+
+  -- セクションの中なら当然アクセスできる
+  #check foo
+end private section
+
+-- `foo` にアクセスできない
+#check_failure foo
+
+/- [^private]: Alex J. Best さんによる [Zulip Chat/lean4/private section](https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/private.20section/near/418114975) での投稿を元にした例です. -/
