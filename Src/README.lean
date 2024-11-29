df5a7c42b4d8f53cbe9cd3ca363689b71b80054a
Seasawher <tellagoodstory@icloud.com>
2024-11-29 23:12:10 +0900

    警告を追加する

--- Src/README.lean
+++ Src/README.lean
@@ -2,5 +2,8 @@
 
 Lean 構文早見表は，Lean の基本的な構文や機能を紹介するためのサンプルコード集です．Lean の数多くの機能の全体を概観できる資料となることを目指しています．
 
-> 本書は[逆引きタクティクリスト](https://lean-ja.github.io/tactic-cheatsheet/)の姉妹編です．mathlibにあるタクティクや，`#` 付きの対話的コマンドについてはあちらを参照してください．
+> **警告**
+>
+> 本書の更新は終了しました。
+> 本書の内容は [Lean by Example](https://lean-ja.github.io/lean-by-example/) に移動されました。
 -/
